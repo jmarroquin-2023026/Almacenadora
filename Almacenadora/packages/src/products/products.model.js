968ab1f2d6850b548a413b00bfc3affdb2e3aab5
dmedina-2023045 <dmedina-2023045@kinal.edu.gt>
2025-05-01 22:29:06 -0600

    se actualiza el modelo de productos

--- Almacenadora/packages/src/products/products.model.js
+++ Almacenadora/packages/src/products/products.model.js
@@ -3,15 +3,18 @@ import { Schema, model } from 'mongoose'
 const productSchema = Schema({
     name: {
         type: String,
-        required: [true, 'Name is required']
+        required: [true, 'Name is required'],
+        maxLength: [25, `Can't be overcome 25 characters`]
     },
     description: {
         type: String,
-        required: [true, 'Description is required']
+        required: [true, 'Description is required'],
+        maxLength: [128, `Can't be overcome 128 characters`]
     },
     brand: {
         type: String,
-        required: [true, 'Branch is required']
+        required: [true, 'Branch is required'],
+        maxLength: [25, `Can't be overcome 25 characters`]
     },
     price: {
         type: Number,
@@ -21,6 +24,10 @@ const productSchema = Schema({
         type: Schema.Types.ObjectId,
         ref: 'Category',
         required: [true, 'Category is required']
+    },
+    date:{
+        type: Date,
+        required: [true, 'Date is required']
     },
     stock: {
         type: Number,
